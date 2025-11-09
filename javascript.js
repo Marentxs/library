22d63da275f8a92c7a2e39cf0a53b7470f9e94eb
Julian <111262742+Marentxs@users.noreply.github.com>
2025-11-09 13:36:38 -0500

    add function bookToLibrary and assigned ID to new books

--- javascript.js
+++ javascript.js
@@ -1,20 +1,22 @@
 const myLibrary = [];
 
-function book(title, author, pages, read) {
+function Book(title, author, pages, read) {
   if (!new.target) {
     throw Error("You must use the 'new operator to call the constructor");
   }
+  this.id = crypto.randomUUID();
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
   this.info = function () {
-    return `${this.title}
-       by   
-      ${this.author}, ${this.pages} 
-       pages, 
-      ${this.read ? "read" : "not read"}`;
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${
+      this.read ? "read" : "not read"
+    }`;
   };
 }
 
-function addBookToLibrary() {}
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+}
