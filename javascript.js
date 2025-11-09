f5e1f9c84a0e11afeff2b2a8292e200fd6c06b02
Julian <111262742+Marentxs@users.noreply.github.com>
2025-11-09 12:51:03 -0500

    add constructor for books

---
+++ javascript.js
@@ -0,0 +1,20 @@
+const myLibrary = [];
+
+function book(title, author, pages, read) {
+  if (!new.target) {
+    throw Error("You must use the 'new operator to call the constructor");
+  }
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return `${this.title}
+       by   
+      ${this.author}, ${this.pages} 
+       pages, 
+      ${this.read ? "read" : "not read"}`;
+  };
+}
+
+function addBookToLibrary() {}
