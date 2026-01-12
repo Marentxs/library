51aa0da7c7f46c21802a8a0a4113032d39507dc8
Julian <111262742+Marentxs@users.noreply.github.com>
2026-01-12 17:55:25 -0500

    add styles for input error

--- javascript.js
+++ javascript.js
@@ -91,31 +91,51 @@ class Controller {
     this.close.addEventListener("click", (event) => {
       this.popup.classList.remove("open");
       event.preventDefault();
+
+      const message = document.getElementById("message");
+      const titleInput = document.getElementById("title");
+      const authorInput = document.getElementById("author");
+      const pagesInput = document.getElementById("pages");
+
+      message.innerHTML = "";
+      titleInput.classList.remove("input-error", "input-success");
+      authorInput.classList.remove("input-error", "input-success");
+      pagesInput.classList.remove("input-error", "input-success");
     });
 
     this.form.addEventListener("submit", (event) => {
       event.preventDefault();
 
       const message = document.getElementById("message");
+      const titleInput = document.getElementById("title");
+      const authorInput = document.getElementById("author");
+      const pagesInput = document.getElementById("pages");
+
       const title = document.getElementById("title").value;
       const author = document.getElementById("author").value;
       const pages = document.getElementById("pages").value;
       const read = document.getElementById("read").checked;
 
       message.innerHTML = "";
+      titleInput.classList.remove("input-error", "input-success");
+      authorInput.classList.remove("input-error", "input-success");
+      pagesInput.classList.remove("input-error", "input-success");
 
       if (title.trim() === "") {
         message.innerHTML = "What's the name of the book?";
+        titleInput.classList.add("input-error");
         return false;
       }
 
       if (author.trim() === "") {
         message.innerHTML = "Who wrote it?";
+        authorInput.classList.add("input-error");
         return false;
       }
 
       if (pages.trim() === "") {
         message.innerHTML = "How long was this book?";
+        pagesInput.classList.add("input-error");
         return false;
       }
 
